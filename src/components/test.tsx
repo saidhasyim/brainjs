93d284605ab734494439024925da1695c3e6c764
Said Hasyim <said@saidhasyim.com>
2024-08-22 09:38:53 +0800

    initial commit

---
+++ src/components/test.tsx
@@ -0,0 +1,7 @@
+//import brain, {type INeuralNetworkJSON, type INeuralNetworkOptions}  from "brain.js";
+import {testFunction} from "../util/brain"
+
+export default function TestPage() {
+    
+  return <>Inside test tsx</>;
+}
---
+++ src/util/brain.ts
@@ -0,0 +1,7 @@
+//import brain, { type INeuralNetworkJSON, type INeuralNetworkOptions } from "brain.js"
+import brain, {type INeuralNetworkJSON, type INeuralNetworkOptions}  from "brain.js";
+
+export async function testFunction() {
+
+  
+}
